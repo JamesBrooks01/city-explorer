76d60430cc98b92896d64de9ab24743a9c322c07
James Brooks <brooksjamesk01@gmail.com>
2022-03-21 17:12:08 -0700

    Added in Form to Query a City and display the Display Name, Latitude and Longitude from Object

--- src/App.js
+++ src/App.js
@@ -1,6 +1,58 @@
+import React from 'react';
+import axios from 'axios';
+import Card from 'react-bootstrap/Card';
 import './App.css';
 
-function App() {
-  
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cityData: {},
+      currentCity: '',
+      dataEntered: false
+    }
+  }
+
+  handleCityNameInput = (e) => {
+    this.setState({
+      currentCity: e.target.value
+    })
+  }
+
+  callCityData = async (e) => {
+    e.preventDefault();
+    let currentCityData = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.currentCity}&format=json`);
+    console.log(currentCityData);
+    this.setState({
+      cityData: currentCityData,
+      dataEntered: true
+    })
+  }
+
+  render() {
+    return (
+      <>
+        <h1>City Explorer</h1>
+        <form onSubmit={this.callCityData}>
+          <label>Pick a city:
+            <input type="text" onInput={this.handleCityNameInput} />
+            <button type="submit">Explore!</button>
+          </label>
+        </form>
+        {this.state.dataEntered
+        ?
+        <Card>
+          <Card.Body>
+            <Card.Title>{this.state.cityData.data[0].display_name}</Card.Title>
+            <Card.Text>Latitude: {this.state.cityData.data[0].lat}</Card.Text>
+            <Card.Text>Longitude: {this.state.cityData.data[0].lon}</Card.Text>
+          </Card.Body>
+        </Card>
+        :
+        <></>
+        }
+      </>
+    );
+  }
 }
 export default App;
