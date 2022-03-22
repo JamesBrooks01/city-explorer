465242b1ebce14ff373334d63861094ff92c788e
James Brooks <brooksjamesk01@gmail.com>
2022-03-21 18:15:08 -0700

    added error message functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import Card from 'react-bootstrap/Card';
+import Alert from 'react-bootstrap/Alert';
 import './App.css';
 
 class App extends React.Component {
@@ -12,7 +13,9 @@ class App extends React.Component {
       latitude: 0,
       longitude: 0,
       imgURL: '',
-      dataEntered: false
+      dataEntered: false,
+      error: false,
+      errorMessage: ''
     }
   }
 
@@ -24,14 +27,21 @@ class App extends React.Component {
 
   callCityData = async (e) => {
     e.preventDefault();
-    let currentCityData = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.currentCity}&format=json`);
-    this.setState({
-      cityData: currentCityData,
-      latitude: currentCityData.data[0].lat,
-      longitude: currentCityData.data[0].lon,
-      imgURL: `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${currentCityData.data[0].lat},${currentCityData.data[0].lon}&zoom=12`,
-      dataEntered: true
-    })
+    try {
+      let currentCityData = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.currentCity}&format=json`);
+      this.setState({
+        cityData: currentCityData,
+        latitude: currentCityData.data[0].lat,
+        longitude: currentCityData.data[0].lon,
+        imgURL: `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${currentCityData.data[0].lat},${currentCityData.data[0].lon}&zoom=12`,
+        dataEntered: true
+      })
+    } catch (error) {
+      this.setState({
+        error: true,
+        errorMessage: `An Error has Occurred: ${error.response.status}`
+      })
+    }
   }
 
   render() {
@@ -39,23 +49,27 @@ class App extends React.Component {
       <>
         <h1>City Explorer</h1>
         <form onSubmit={this.callCityData}>
-          <label>Pick a city:</label>
-            <input type="text" onInput={this.handleCityNameInput} />
-            <button type="submit">Explore!</button>
+          <label>Type a City Name:</label>
+          <input type="text" onInput={this.handleCityNameInput} />
+          <button type="submit">Explore!</button>
         </form>
-        {this.state.dataEntered
-        ?
-        <Card>
-          <Card.Body>
-            <Card.Title>{this.state.cityData.data[0].display_name}</Card.Title>
-            <Card.Text>Latitude: {this.state.latitude}</Card.Text>
-            <Card.Text>Longitude: {this.state.longitude}</Card.Text>
-            <Card.Img src={this.state.imgURL}/>
-          </Card.Body>
-        </Card>
-        :
-        <></>
-        }
+        {this.state.error
+          ?
+          <Alert id='error-message'>{this.state.errorMessage}</Alert>
+          :
+              this.state.dataEntered
+                ?
+                <Card>
+                  <Card.Body>
+                    <Card.Title>{this.state.cityData.data[0].display_name}</Card.Title>
+                    <Card.Text>Latitude: {this.state.latitude}</Card.Text>
+                    <Card.Text>Longitude: {this.state.longitude}</Card.Text>
+                    <Card.Img src={this.state.imgURL} />
+                  </Card.Body>
+                </Card>
+                :
+                <></>
+            }
       </>
     );
   }
